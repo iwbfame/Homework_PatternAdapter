873ca0a89d882a1d4a7b089a02dfcecc90891a4f
Va test <arcticque421@gmail.com>
2024-01-08 23:15:09 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(5,10));
+        System.out.println(calc.pow(10,5));
+        System.out.println(calc.sum(100,222));
     }
 }
